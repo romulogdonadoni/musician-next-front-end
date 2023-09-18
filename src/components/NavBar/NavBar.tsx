75781ec1d147c5b0656764af867b0f919c2bb7bf
romulogdonadoni <romulogdonadoni@gmail.com>
2023-09-18 15:14:12 -0300

    2.1

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,27 @@
+"use client";
 
 import { TbMusicUp } from "react-icons/tb";
 import { CgProfile } from "react-icons/cg";
 import Link from "next/link";
 import NavHistory from "./NavHistory";
-import { cookies } from "next/headers";
+import { getCookie, hasCookie } from "cookies-next";
+import jwtDecode from "jwt-decode";
+import { useEffect, useState } from "react";
+
+type UserType = {
+  id: string;
+  username: string;
+  iat: number;
+};
+
 export default function NavBar() {
-  const cookiesStorage = cookies();
+  const [user, setUser] = useState<UserType>();
+  const token = getCookie("auth-token");
+
+  useEffect(() => {
+    setUser(jwtDecode(token!));
+  }, [token]);
+
   return (
     <nav className="flex justify-between items-center ">
       <NavHistory />
@@ -15,11 +31,7 @@ export default function NavBar() {
           <TbMusicUp size={26} color={"#FF4C29"} />
         </div>
         <Link href={"/auth/login"} className="flex gap-2 py-2 px-3 rounded-full cursor-pointer">
-          {cookiesStorage.has("auth-token") ? (
-            <span className="text-orange font-bold">{"Sair"}</span>
-          ) : (
-            <span className="text-orange font-bold">{"Entrar"}</span>
-          )}
+          <span className="text-orange font-bold">{user?.username ? user.username : "Entrar"}</span>
 
           <CgProfile size={26} color={"#FF4C29"} />
         </Link>
